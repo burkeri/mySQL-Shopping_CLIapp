b8b3ed92e3fde578a2776731a4c8445903db3a82
Richelle Burke <burkeri@alumni.upenn.edu>
2018-12-11 22:13:53 -0500

    building out functions

--- bamazon_Main.js
+++ bamazon_Main.js
@@ -12,75 +12,74 @@ var connection = mysql.createConnection({
     database: "bamazon_DB"
 });
 
-// query search vairables
+// query search variables
 var products = "SELECT item_id, product_name, department_name, price FROM products";
 var byDep = "SELECT item_id, product_name, department_name, price FROM products WHERE department_name = ?";
+var byID = "SELECT item_id, product_name, department_name, price FROM products WHERE item_id BETWEEN ? AND ?";
+var byPrice = "SELECT item_id, product_name, department_name, price FROM products WHERE price BETWEEN ? AND ?";
 
 // functions
+// customer view of products
+function customerView(err, res){
+
+    if (err) throw err;
+
+    var newRow, 
+        data, 
+        output;
+    
+    data = [
+        ["ID", "Item", "Department", "Price (dollars)"],
+    ];
+
+    for (i=0; i<res.length; i++){
+        newRow = [];
+        newRow.push(
+            res[i].item_id,
+            res[i].product_name,
+            res[i].department_name,
+            res[i].price
+        );
+        data.push(newRow);
+        newRow = [];
+    };
+
+    output = table(data);
+    console.log(output);
+
+    connection.end(); 
+}
+
 // view all products
 function viewProducts(){
-
-    connection.query(products, function(err, res){     
-        
-        if (err) throw err;
-
-        var newRow, 
-            data, 
-            output;
-        
-        data = [
-            ["ID", "Item", "Department", "Price (dollars"],
-        ];
-
-        for (i=0; i<res.length; i++){
-            newRow = [];
-            newRow.push(
-                res[i].item_id,
-                res[i].product_name,
-                res[i].department_name,
-                res[i].price
-            );
-            data.push(newRow);
-            newRow = [];
-        };
-
-        output = table(data);
-        console.log(output);
-
-        connection.end();
+    connection.query(products, function(err, res){       
+        customerView(err, res); 
     });
-};
+}
 
 // shop products by department
 function shopByDep(){
-
-    connection.query(byDep, function(err, res){     
-        
-        if (err) throw err;
-
-        var newRow, 
-            data, 
-            output;
-        
-        data = [
-            ["ID", "Item", "Department", "Price (dollars"],
-        ];
-
-        for (i=0; i<res.length; i++){
-            newRow = [];
-            newRow.push(
-                res[i].item_id,
-                res[i].product_name,
-                res[i].department_name,
-                res[i].price
-            );
-            data.push(newRow);
-            newRow = [];
-        };
-
-        output = table(data);
-        console.log(output);
-
-        connection.end();
+    connection.query(byDep, process.argv[2], function(err, res){     
+        customerView(err, res);
     });
-};
+}
+
+// shop products by id
+function shopByID(){
+    connection.query(byID, process.argv[0], function(err, res){
+        customerView(err, res);
+    });
+}
+
+// shop products by price
+function shopByPrice(){
+    connection.query(byPrice, process.argv[0], function(err, res){
+        customerView(err, res);
+    });
+}
+
+inquirer.prompt([
+    {
+        name
+    }
+]).then({});
